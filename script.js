7f195668f9d7895a9d00182b5c258c76cbd6c589
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-06-02 11:43:29 +0200

    bugfix

--- script.js
+++ script.js
@@ -23,27 +23,22 @@ for (let i = 0; i < layers.length; i++) { //for every layer, repeat the followin
     }
     if (layers[i].id == "layer1") //if it's the the snail
     {
-         addSpecialButtons(buttonContainer, "path899", "path885", color1, "spin");//add the animation-buttons
-    } 
+        addSpecialButtons(buttonContainer, "path899", "path885", color1, "spin"); //add the animation-buttons
+    }
     section.appendChild(buttonContainer); //put the span-element inside the section-element
 }
 
-//detects clicks on buttons and runs the function showPart
+//detects clicks on buttons and shows or hides lines
 section.addEventListener("click", (e) => {
-    showPart(e.target.classList)
-});
-
-//makes the lines show up or hide
-function showPart(clicked) {
     for (let i = 0; i < allLines.length; i++) {
-        clicked.toggle("on");//changes button look
-        if (allLines[i].classList.contains(clicked)) {
+        if (allLines[i].classList.contains(e.target.classList[0])) {
             allLines[i].classList.toggle("hidden");
         }
     }
-}
+    e.target.classList.toggle("on"); //changes button look
+});
 
-function addSpecialButtons(container, line1, line2, color, animation) { 
+function addSpecialButtons(container, line1, line2, color, animation) {
     //adds a buttons that changes color of a part when clicked
     let colorButton = document.createElement("button");
     colorButton.innerText = "¤"; //here you can change the button text
@@ -56,11 +51,12 @@ function addSpecialButtons(container, line1, line2, color, animation) {
         }
     });
     //adds a button that adds an animation when clicked
-    if(animation)
-    {let animationButton = document.createElement("button");
-    animationButton.innerText = "¤"
-    animationButton.addEventListener("click", () => {
-        document.getElementById(line2).classList.toggle(animation)
-    })
-    container.append(animationButton, colorButton);}
+    if (animation) {
+        let animationButton = document.createElement("button");
+        animationButton.innerText = "¤"
+        animationButton.addEventListener("click", () => {
+            document.getElementById(line2).classList.toggle(animation)
+        })
+        container.append(animationButton, colorButton);
+    }
 }
